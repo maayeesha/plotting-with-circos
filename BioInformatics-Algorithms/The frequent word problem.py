84423c319a04704334e6ccafb432a5006905c890
Maayeesha Farzana <63303436+maayeesha@users.noreply.github.com>
2024-10-08 15:41:59 +0600

    The frequent word problem.py

---
+++ BioInformatics-Algorithms/The frequent word problem.py
@@ -0,0 +1,13 @@
+def FrequencyMap(Text, k):
+    freq = {}
+    n = len(Text)
+    for i in range(n-k+1):
+        Pattern = Text[i:i+k]
+        freq[Pattern] = 0
+    for i in range(n-k+1):
+        Pattern = Text[i:i+k]
+        if Pattern in freq.keys():
+            freq[Pattern] += 1
+        else:
+          freq[Pattern] = 1
+    return freq

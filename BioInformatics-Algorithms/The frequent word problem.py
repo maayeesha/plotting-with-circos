c82be936f5285082d3a68980e93cafbfac5adbd3
Maayeesha Farzana <63303436+maayeesha@users.noreply.github.com>
2024-10-08 16:10:28 +0600

    Update The frequent word problem.py

--- BioInformatics-Algorithms/The frequent word problem.py
+++ BioInformatics-Algorithms/The frequent word problem.py
@@ -1,3 +1,15 @@
+# Input:  A string Text and an integer k
+# Output: A list containing all most frequent k-mers in Text
+def FrequentWords(Text, k):
+    words = []
+    freq = FrequencyMap(Text, k)
+    m = max(freq.values())
+    for key in freq:
+        if freq[key] == m:
+            words.append(key)
+    return words
+
+# Copy your FrequencyMap() function here.
 def FrequencyMap(Text, k):
     freq = {}
     n = len(Text)
